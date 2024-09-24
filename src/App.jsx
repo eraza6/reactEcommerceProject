f6615818708f4934b944eefff927d838385f9761
Elvis Raza <eraza.a@hotmail.com>
2024-09-23 21:40:59 -0500

    CreaTuLanding1+Raza

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import Navbar from './components/Navbar.jsx';
+import ProductContainer from './components/ProductContainer.jsx';
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navbar />
+      <ProductContainer />
+    </>
+  )
+}
+
+export default App
